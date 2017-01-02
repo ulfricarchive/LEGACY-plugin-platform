21daf57a8f815fe6793ff6883fae4a618378d429
Adam Harrison <ad@mharrison.us>
2017-01-02 14:07:15 -0500

    tests

---
+++ src/test/java/com/ulfric/plugin/commons/CommonsTest.java
@@ -0,0 +1,13 @@
+package com.ulfric.plugin.commons;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import com.ulfric.commons.spigot.testing.plugin.JavaPluginTestBase;
+import com.ulfric.commons.spigot.testing.plugin.Plugin;
+
+@RunWith(JUnitPlatform.class)
+@Plugin(Commons.class)
+class CommonsTest extends JavaPluginTestBase {
+
+}
