9587b6d1e6047c179fac83694ca978054d46d716
insou22 <zac.kologlu@gmail.com>
2017-01-10 13:13:17 +1100

    Migrate plugin-commons to register BeanFactory

--- src/main/java/com/ulfric/plugin/commons/Commons.java
+++ src/main/java/com/ulfric/plugin/commons/Commons.java
@@ -1,7 +1,15 @@
 package com.ulfric.plugin.commons;
 
+import com.ulfric.commons.cdi.construct.BeanFactory;
 import com.ulfric.commons.spigot.plugin.UlfricPlugin;
+import com.ulfric.commons.spigot.service.ServiceUtils;
 
-public class Commons extends UlfricPlugin {
+public final class Commons extends UlfricPlugin {
+
+	@Override
+	public void init()
+	{
+		ServiceUtils.registerIfAbsent(BeanFactory.class, BeanFactory::newInstance);
+	}
 
 }
