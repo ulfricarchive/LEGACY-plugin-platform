d049d1d0c0594b32b206fe485f95d55e7c57248a
Adam Harrison <ad@mharrison.us>
2017-02-18 03:06:29 -0500

    Update for upstream changes

--- src/main/java/com/ulfric/plugin/platform/Platform.java
+++ src/main/java/com/ulfric/plugin/platform/Platform.java
@@ -7,6 +7,7 @@ import org.bukkit.event.Listener;
 import com.ulfric.commons.cdi.ObjectFactory;
 import com.ulfric.commons.cdi.container.Container;
 import com.ulfric.commons.cdi.container.Feature;
+import com.ulfric.commons.cdi.initialize.Initialize;
 import com.ulfric.commons.spigot.cdi.scope.service.Service;
 import com.ulfric.commons.spigot.cdi.scope.service.ServiceScopeStrategy;
 import com.ulfric.commons.spigot.container.ContainerLogger;
@@ -15,19 +16,17 @@ import com.ulfric.commons.spigot.container.ServiceFeature;
 import com.ulfric.commons.spigot.plugin.UlfricPlugin;
 import com.ulfric.commons.spigot.service.ServiceUtils;
 
-public final class Platform extends UlfricPlugin {
+public class Platform extends UlfricPlugin {
 
 	private ObjectFactory global;
 
-	@Override
-	protected void setupPlatform()
+	@Initialize
+	void setupPlatform()
 	{
 		this.registerGlobalObjectFactory();
 		this.registerScopes();
 		this.registerBindings();
 		this.registerComponents();
-
-		super.setupPlatform();
 	}
 
 	private void registerGlobalObjectFactory()
