3dd2887843b00c13769d5170b0eec281449f68bc
Adam Harrison <ad@mharrison.us>
2017-04-15 14:06:33 -0400

    Fix rule placeholder

--- src/main/java/com/ulfric/plugin/platform/placeholder/RulePlaceholder.java
+++ src/main/java/com/ulfric/plugin/platform/placeholder/RulePlaceholder.java
@@ -2,9 +2,11 @@ package com.ulfric.plugin.platform.placeholder;
 
 import org.bukkit.command.CommandSender;
 
+import com.ulfric.commons.naming.Name;
 import com.ulfric.commons.spigot.metadata.Metadata;
 import com.ulfric.commons.spigot.text.placeholder.Placeholder;
 
+@Name("RULE_FAILED")
 public class RulePlaceholder implements Placeholder {
 
 	@Override
